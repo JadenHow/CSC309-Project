5d4c6bb9eeadc3ef12bd009828ac881756341212
weijinluo <weijinluo@gmail.com>
2022-12-09 02:59:17 -0500

    plz

---
+++ PF/CSC309-Project/backend/backend/classes/views.py
@@ -0,0 +1,311 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
+from studios.models import Studio
+from classes.models import ClassInstances        
+from classes.serializers import ClassInstancesSerializer
+from rest_framework import generics, permissions
+from knox.auth import TokenAuthentication
+import datetime
+from users.models import Enrolled, RegisterUser
+from subscriptions.models import SubscriptionInstance
+from users.serializers import EnrolledSerializer
+from . import client
+from rest_framework.pagination import LimitOffsetPagination
+
+
+class EnrolAPIView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes= [permissions.IsAuthenticated]
+
+    def post(self, request, studio_id, class_id):
+        # /studios/classes/<classid>/enrol
+        # studio = get_object_or_404(Studio, pk=studio_id)
+        class_instance = get_object_or_404(ClassInstances, pk=class_id)
+
+        if not get_object_or_404(RegisterUser, user=request.user.id).subscribed:
+            return Response({"msg" : "not subscribed"}, status=404)
+
+        if get_object_or_404(SubscriptionInstance, user=request.user.id).renewal_date < class_instance.class_date:
+            return Response({"msg" : "subscription will be expired before the class, please enrol after your subscription is renewed or subscribe to a longer subscription"}, status=404)
+
+        # print(request.user)
+        curr_time = datetime.datetime.now().time()
+        today = datetime.date.today()
+
+        # can't already be enrolled
+        if Enrolled.objects.filter(class_instance = class_id, user = request.user.id).exists():
+            print(Enrolled.objects.filter(class_instance = class_id, user = request.user.id))
+            return Response({
+                "msg": "Already enrolled" 
+            }, status=404)
+
+
+        if class_instance.currently_enrolled < class_instance.capacity and class_instance.class_date >= today:
+            if class_instance.class_date == today and class_instance.start_time <= curr_time:
+                return Response({
+                    "msg": "Class has already passed" 
+                    })
+
+            data = {
+                'user': request.user.id,
+                'class_instance': class_id
+            }
+
+            serializer = EnrolledSerializer(data=data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                class_instance.currently_enrolled += 1
+                class_instance.save()
+
+            return Response({
+                "msg": "Successfully Enrolled" 
+            }, status=200)
+        else:
+            return Response({"msg" : "Doens't work"}, status=404)
+
+
+class DropAPIView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+    def post(self, request, studio_id, class_id):
+        if not ClassInstances.objects.filter(parent_class=class_id).exists:
+            return Response({"msg" : "Class doesn't exist"}, status=404)
+
+        print(class_id, request.user.id)
+        enrolled_instance = Enrolled.objects.filter(class_instance=class_id, user = request.user.id)
+        print(enrolled_instance)
+        if enrolled_instance.exists():
+            enrolled_instance.delete()
+
+            # subract currently enrolled
+            class_instance = get_object_or_404(ClassInstances, pk=class_id)
+            class_instance.currently_enrolled -= 1
+            class_instance.save() 
+
+            return Response({
+                "msg": "Successfully dropped" 
+            }, status=200)
+        else: 
+            return Response({
+                "msg": "Not enrolled" 
+            }, status=404)
+
+
+class ClassSearchAPIView(generics.ListAPIView):
+    pagination_class = LimitOffsetPagination
+    serializer_class = ClassInstancesSerializer
+    queryset = ''
+    
+    def get(self, request, *args, **kwargs):
+        query = request.GET.get('q')
+        start = request.GET.get('start') or None
+        end = request.GET.get('end') or None
+        starttime = request.GET.get('starttime') or None
+        endtime = request.GET.get('endtime') or None
+
+        results = client.perform_search(query, start=start, end=end, starttime=starttime, endtime=endtime)
+        # print(results['hits'])
+        page = self.paginate_queryset(results['hits'])
+        return self.get_paginated_response(page)
+
+class_search_view = ClassSearchAPIView.as_view()
+
+class EnrolMultipleAPIView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes= [permissions.IsAuthenticated]
+
+    def post(self, request, studio_id, class_id):
+        # studio = get_object_or_404(Studio, pk=studio_id)
+        class_instance = get_object_or_404(ClassInstances, pk=class_id)
+
+        if not get_object_or_404(RegisterUser, user=request.user.id).subscribed:
+            return Response({"msg" : "not subscribed"}, status=404)
+
+        if get_object_or_404(SubscriptionInstance, user=request.user.id).renewal_date < class_instance.class_date:
+            return Response({"msg" : "subscription will be expired before the class, please enrol after your subscription is renewed or subscribe to a longer subscription"}, status=404)
+
+        curr_time = datetime.datetime.now().time()
+        today = datetime.date.today()
+
+        all_class_instances = ClassInstances.objects.filter(parent_class = class_instance.parent_class)
+
+        for class_instance in all_class_instances:
+            # Can't already be enrolled
+            if Enrolled.objects.filter(class_instance = class_instance.pk, user = request.user.id).exists():
+                continue
+            if class_instance.currently_enrolled < class_instance.capacity and class_instance.class_date >= today:
+                if class_instance.class_date == today and class_instance.start_time <= curr_time:
+                    continue
+            if get_object_or_404(SubscriptionInstance, user=request.user.id).renewal_date < class_instance.class_date:
+                continue
+            
+            data = {
+                'user': request.user.id,
+                'class_instance': class_instance.pk
+            }
+
+            serializer = EnrolledSerializer(data=data)
+            if serializer.is_valid(raise_exception=True):
+                serializer.save()
+                class_instance.currently_enrolled += 1
+                class_instance.save()
+
+        return Response({
+            "msg": "Successfully enrolled to all future occurrences" 
+        }, status=200)
+
+class DropMultipleAPIView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+    def post(self, request, studio_id, class_id):
+        if not ClassInstances.objects.filter(parent_class=class_id).exists:
+            return Response({"msg" : "Class doesn't exist"}, status=404)
+        
+        class_instance = get_object_or_404(ClassInstances, pk=class_id)
+
+        all_class_instances = ClassInstances.objects.filter(parent_class = class_instance.parent_class)
+
+        for class_instance in all_class_instances:
+            enrolled_instance = Enrolled.objects.filter(class_instance=class_instance.pk, user = request.user.id)
+        
+            if enrolled_instance.exists():
+                enrolled_instance.delete()
+
+                # subract currently enrolled
+                class_instance_object = get_object_or_404(ClassInstances, pk=class_instance.pk)
+                class_instance_object.currently_enrolled -= 1
+                class_instance_object.save() 
+
+        return Response({
+            "msg": "Successfully dropped all future occurrences" 
+        }, status=200)
+
+# class CancelAllClasses(APIView):
+#     authentication_classes = [TokenAuthentication]
+#     permission_classes = [permissions.IsAdminUser]
+
+#     def post(self, request, studio_id, class_id):
+
+#         curr_studio = get_object_or_404(Studio, pk=studio_id)
+#         if curr_studio.owner != request.user.id:
+#             return Response({"msg" : "Permission denied"}, status=403)
+
+#         curr_class = get_object_or_404(Class, pk=class_id, studio = studio_id)
+#         curr_class.cancelled = True
+#         curr_class.save()
+
+#         queryset = ClassInstances.objects.all().filter(parent_class = curr_class.pk)
+
+#         for obj in queryset:
+#             obj.cancelled = True
+#             obj.save()
+
+#         return Response({"msg" : "Successfully cancelled"}, status=200)
+
+
+# class UpdateClassAPIView(APIView):
+#     authentication_classes = [TokenAuthentication]
+#     permission_classes = [permissions.IsAdminUser]
+
+#     def patch(self, request, studio_id, class_id):
+        
+#         curr_studio = get_object_or_404(Studio, pk=studio_id)
+#         # print(curr_studio.owner)
+#         # print(request.user)
+
+#         if curr_studio.owner != request.user.id:
+#             return Response({"msg" : "Permission denied"}, status=403)
+
+#         data = request.data
+
+#         curr_class = get_object_or_404(Class, pk=class_id)
+#         serializer = ClassSerializer(curr_class, data = data, partial = True)
+
+#         if serializer.is_valid():
+#             serializer.save()
+
+#             return Response({"msg" : "Successfully edited"}, status=200)
+
+
+# Create your views here.
+# class ClassCreateApiView(APIView):
+#     authentication_classes = [TokenAuthentication]
+#     permission_classes = [permissions.IsAdminUser]
+
+#     def post(self, request, studio_id):
+#         if Studio.objects.filter(pk=studio_id).exists():
+
+#             studio = Studio.objects.get(pk=studio_id)
+#             if studio.owner != request.user:
+#                 return Response({"msg" : "Permission denied"}, status=403)
+
+#             # 'studio',
+#             name = request.data.get("name")
+#             description = request.data.get("description")
+#             coach = request.data.get("coach")
+#             keywords = request.data.get("keywords")
+#             capacity = request.data.get("capacity")
+#             start_date = request.data.get("start_date")
+#             start_time = request.data.get("start_time")
+#             end_time = request.data.get("end_time")
+#             end_recursion = request.data.get("end_recursion")
+
+#             data = {
+#                 'studio': studio_id,
+#                 'name': name,
+#                 'description': description,
+#                 'coach': coach,
+#                 'keywords': keywords,
+#                 'capacity': capacity,
+#                 'start_date': start_date,
+#                 'start_time': start_time,
+#                 'end_time': end_time,
+#                 'end_recursion': end_recursion
+#             }
+
+#             serializer = ClassSerializer(data = data)
+#             if serializer.is_valid(raise_exception=True):
+#                 created_class = serializer.save()
+
+#                 curr_date = created_class.start_date
+#                 end = created_class.end_recursion
+
+#                 while curr_date <= end:
+#                     curr_date_list = str(curr_date).split('-')
+#                     start_time_list = start_time.split(':')
+#                     start_datetime = datetime.datetime(int(curr_date_list[0]), int(curr_date_list[1]), int(curr_date_list[2]), int(start_time_list[0]), int(start_time_list[1]))
+#                     start_datetime_unix = time.mktime(start_datetime.timetuple())
+                    
+#                     start_time_timestamp = start_time.split(':')
+#                     start_time_timestamp = start_time_timestamp[0] + start_time_timestamp[1] + start_time_timestamp[2]
+
+#                     instance_data = {
+#                         'studio': studio_id,
+#                         'parent_class': created_class.pk,
+#                         'name': name,
+#                         'description': description,
+#                         'coach': coach,
+#                         'keywords': keywords,
+#                         'capacity': capacity,
+#                         'class_date': curr_date,
+#                         'class_date_timestamp': start_datetime_unix,
+#                         'start_time': start_time,
+#                         'start_time_timestamp' : start_time_timestamp,
+#                         'end_time': end_time,
+#                     }
+
+#                     instance_serializer = ClassInstancesSerializer(data=instance_data)
+#                     if instance_serializer.is_valid(raise_exception=True):
+#                         instance_serializer.save()
+
+#                     curr_date += timedelta(days=7)
+
+#                 return JsonResponse({
+#                     "works": "works" 
+#                 })
+                    
+#         return JsonResponse({
+#                 "studio_id": "NOT REAL" 
+#             })
+
---
+++ PF/CSC309-Project/backend/backend/studios/views.py
@@ -0,0 +1,159 @@
+from . import client
+from .models import Studio
+from .serializers import StudioSerializer
+from classes.models import ClassInstances
+from classes.serializers import ClassInstancesSerializer
+from django.shortcuts import get_object_or_404
+from geopy.distance import geodesic as GD
+from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+import datetime
+from rest_framework.pagination import LimitOffsetPagination
+
+# Create your views here.
+# class StudioCreateAPIView(APIView):
+#     parser_classes = (MultiPartParser, FormParser)
+#     authentication_classes = [TokenAuthentication]
+#     permission_classes = [permissions.IsAdminUser]
+#     def post(self, request):
+#         data = {
+#             'owner': request.user.id,
+#             'name': request.data.get("name"),
+#             'phone_number': request.data.get("phone_number"),
+#             'address': request.data.get("address"),
+#             'postal_code': request.data.get("postal_code"),
+#             'latitude': request.data.get("latitude"),
+#             'longitude': request.data.get("longitude")
+#         }
+        
+#         uploaded_images = request.FILES.getlist('uploaded_images')
+#         serializer = StudioSerializer(data=data)
+#         if serializer.is_valid(raise_exception=True):
+#             new_studio = serializer.save()
+#             print(new_studio)
+#             for image in uploaded_images:
+#                 img_data = {
+#                     'studio': new_studio,
+#                     'image': image
+#                 }
+#                 image_serializer = StudioImageSerializer(data=img_data)
+#                 if image_serializer.is_valid(raise_exception=True):
+#                         image_serializer.save()
+#                 else:
+#                     return Response({
+#                         "Status": "BAD IMAGE",
+#                     })  
+#             return Response({
+#                 "Status": "200OK",
+#             })
+#         else:
+#             return Response({
+#                 "Status": "NOT OK",
+#             })
+
+class NearbyStudioListAPIView(generics.ListAPIView):
+    pagination_class = LimitOffsetPagination
+    serializer_class = StudioSerializer
+
+    def get(self, request):
+        user_latitude = request.data.get("latitude")
+        user_longitude = request.data.get("longitude")
+
+        queryset = Studio.objects.all()
+        data = StudioSerializer(queryset, many=True, context={'request': request}).data
+        
+        list_of_studios = []
+        for obj in data:
+            studio_latitude = obj["latitude"]
+            studio_longitude = obj["longitude"]
+
+            user_location = (user_latitude, user_longitude)
+            studio_location = (studio_latitude, studio_longitude)
+
+            distance = GD(user_location, studio_location).km
+            list_of_studios.append((distance, obj))
+        
+        list_of_studios.sort(key=lambda tup: tup[0])
+        new_list = []
+        for obj_new in list_of_studios:
+            data = {
+                'pk': obj_new[1]['pk'],
+                'owner': obj_new[1]['owner'],
+                'name': obj_new[1]['name'],
+                'phone_number': obj_new[1]['phone_number'],
+                'address': obj_new[1]['address'],
+                'postal_code': obj_new[1]['postal_code'],
+                'latitude': obj_new[1]['latitude'],
+                'longitude': obj_new[1]['longitude'],
+            }
+            new_list.append(data)
+        
+        page = self.paginate_queryset(new_list)
+        return self.get_paginated_response(page)
+
+class StudioListAPIView(generics.ListAPIView):
+    queryset = Studio.objects.all()
+    serializer_class = StudioSerializer
+
+class StudioDetailAPIView(generics.RetrieveAPIView):
+    queryset = Studio.objects.all()
+    serializer_class = StudioSerializer
+
+class ClassListAPIView(generics.ListAPIView):
+    pagination_class = LimitOffsetPagination
+    serializer_class = ClassInstancesSerializer
+
+    def get(self, request, pk):
+        studio_obj = get_object_or_404(Studio, pk=pk)
+
+        queryset_future_date = ClassInstances.objects.filter(studio=studio_obj.pk, cancelled = False, class_date__gt = datetime.date.today())
+        queryset_today = ClassInstances.objects.filter(studio=studio_obj.pk, cancelled = False, class_date = datetime.date.today(), start_time__gte = datetime.datetime.now().time())
+
+        # union of the two querysets
+        cleaned_data = queryset_future_date | queryset_today
+        cleaned_data = cleaned_data.order_by("class_date", "start_time")
+
+        # print(cleaned_data)
+
+        data = ClassInstancesSerializer(cleaned_data, many=True).data
+        page = self.paginate_queryset(data)
+        return self.get_paginated_response(page)
+
+class StudioSearchAPIView(generics.ListAPIView):
+    pagination_class = LimitOffsetPagination
+    serializer_class = StudioSerializer
+    queryset = ''
+
+    def get(self, request, *args, **kwargs):
+        query = request.GET.get('q')
+        studio = request.GET.get('studio')
+        # amenities = request.GET.get('amenities')
+        # classes = request.GET.get('classes')
+        if not query:
+            return Response('', status=400)
+
+        # studio_results = client.perform_search(query, 'backend_Studio')
+        # studio_amenities_results = client.perform_search(query, 'backend_StudioAmenities')
+        # classes_results = client.perform_search(query, 'backend_Class')
+    
+        # if studios:
+        #     studio_results = client.perform_search(studios, 'backend_Studio')
+        # if amenities:
+        #     studio_amenities_results = client.perform_search(amenities, 'backend_StudioAmenities')
+        # if classes:
+        #     classes_results = client.perform_search_studio_class(classes)
+        classes_results = client.perform_search_studio_class(query, studio)
+        
+        # all_data = []
+        # for data in studio_results['hits']:
+        #     all_data.append(data)
+        
+        # for data in studio_amenities_results['hits']:
+        #     all_data.append(data)
+        
+        # for data in classes_results['hits']:
+        #     all_data.append(data)
+        
+        page = self.paginate_queryset(classes_results['hits'])
+        return self.get_paginated_response(page)
