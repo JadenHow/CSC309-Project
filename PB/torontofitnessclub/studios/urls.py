4056fcadb6032d39634ce101f4821c7319ff1090
JadenHow <jadenhow02@gmail.com>
2022-12-08 20:42:24 -0500

    delete pb

--- PB/torontofitnessclub/studios/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import StudioListView, StudioDetailView
-
-urlpatterns = [
-    path('', StudioListView.as_view(), name='studio_list'),
-    path('<int:pk>/', StudioDetailView.as_view(), name='studio_detail'),
-]
--- PB/torontofitnessclub/subscriptions/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-import subscriptions.views as views
-urlpatterns = [
-    path('', views.SubscriptionListView.as_view(), name='subscription_list'),
-    path('payments/', views.UserPaymentsView.as_view(), name='payments_list'),
-    path('<int:pk>/subscribe/', views.UserSubscribeView.as_view(), name='subscribe_view'),
-    path('<int:pk>/unsubscribe/', views.UserUnsubscribeView.as_view(), name='unsubscribe_view'),
-]
